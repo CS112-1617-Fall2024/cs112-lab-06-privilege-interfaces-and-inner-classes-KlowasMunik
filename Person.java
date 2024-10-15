9e199c82a182b9a3c61172c739e767073ba59e5d
KlowasMunik <78110073+KlowasMunik@users.noreply.github.com>
2024-10-15 06:52:11 +0000

    munik

--- Person.java
+++ Person.java
@@ -1,85 +1,123 @@
-public class Person
-{
-	/***** TODO: (Part 2) create helper inner class for Identity*****/
+public class Person implements Comparable<Person> {
+    // CONSTANT VARIABLES
+    public static final String DEFAULT_NAME = "Jamie Doe";
+    public static final String DEFAULT_STORY = "Unknown";
+    public static final int DEFAULT_PRIVILEGE = 100;
 
-	// CONSTANT VARIABLES
-	public static final String DEFAULT_NAME = "Jamie Doe";
-	public static final String DEFAULT_STORY =  "Unknown";
-	public static final int DEFAULT_PRIVILEGE = 100;
+    // INSTANCE VARIABLES
+    private String name;
+    private Identity identity;  // Changed from 'story' to 'identity'
+    private int privilege;
 
-	// INSTANCE VARIABLES
-	private String name, story;
-	private int privilege;
+    /**
+     * Identity inner class to represent pronouns and background
+     */
+    public class Identity {
+        private String pronouns;
+        private String background;
 
-	// CONSTRUCTORS	
-	public Person(String name, String story, int privilege) {
-		this.setAll(name, story, privilege);
-	}
-		
-	public Person() {
-		this(DEFAULT_NAME, DEFAULT_STORY, DEFAULT_PRIVILEGE);
-	}
-	
-	public Person(Person original) {
-		if(original == null) {
-			throw new IllegalArgumentException("Cannot copy null obect in Person copy constructor");
-		} else {
-			this.setAll(original.name, original.story, original.privilege);
-		}
-	}
+        public Identity(String pronouns, String background) {
+            this.pronouns = pronouns;
+            this.background = background;
+        }
 
-	// MUTATORS/SETTERS
-	public void setName(String name) {
-		this.name = name;
-	}
+        // Setters
+        public void setPronouns(String pronouns) {
+            this.pronouns = pronouns;
+        }
 
-	public void setStory(String story) {
-		this.story = story;
-	}
+        public void setBackground(String background) {
+            this.background = background;
+        }
 
-	public void setPrivilege(int privilege) {
-		this.privilege = privilege;
-	}
+        // Getters
+        public String getPronouns() {
+            return pronouns;
+        }
 
-	public void setAll(String name, String story, int privilege) {
-		this.setPrivilege(privilege);
-		this.setName(name);
-		this.setStory(story);
-	}
+        public String getBackground() {
+            return background;
+        }
 
-	// ACCESSORS / GETTERS
-	public String getName() {
-		return this.name;
-	}
-		
-	public String getStory() {
-		return this.story;
-	}
+        @Override
+        public String toString() {
+            return "Pronouns: " + pronouns + ", Background: " + background;
+        }
+    }
 
-	public int getPrivilege() {
-		return this.privilege;
-	}
+    // CONSTRUCTORS
+    public Person(String name, String pronouns, String background, int privilege) {
+        this.name = name;
+        this.identity = new Identity(pronouns, background);
+        this.privilege = privilege;
+    }
 
-	// OTHER REQUIRED METHODS
-	@Override
-	public String toString()
-	{
-		return "My name is "+ this.name + " and "+ this.story + "\n"
-				+ "According to this calculator I ended up with "+ this.privilege + " estimated privilege points";
-	}
-	
-	@Override
-	public boolean equals(Object other) 
-	{
-		if(other == null || (!(other instanceof Person))) {
-		      return false;
-		}
-		
-		Person otherPerson = (Person) other;
-		return this.name.equals(otherPerson.name) && this.story.equals(otherPerson.story) &&
-			this.privilege == otherPerson.privilege;	
-	}
+    public Person() {
+        this(DEFAULT_NAME, "They/Them", DEFAULT_STORY, DEFAULT_PRIVILEGE);
+    }
 
-	// INTERFACE METHODS
-	/***** TODO: (Part 1) override compareTo method to implement Comparable interface*****/
+    public Person(Person original) {
+        if (original == null) {
+            throw new IllegalArgumentException("Cannot copy null object in Person copy constructor");
+        } else {
+            this.name = original.name;
+            this.identity = new Identity(
+                original.identity.getPronouns(),
+                original.identity.getBackground()
+            );
+            this.privilege = original.privilege;
+        }
+    }
+
+    // SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrivilege(int privilege) {
+        this.privilege = privilege;
+    }
+
+    public void setIdentity(String pronouns, String background) {
+        this.identity.setPronouns(pronouns);
+        this.identity.setBackground(background);
+    }
+
+    // GETTERS
+    public String getName() {
+        return name;
+    }
+
+    public int getPrivilege() {
+        return privilege;
+    }
+
+    public Identity getIdentity() {
+        return identity;
+    }
+
+    // INTERFACE METHODS: Comparable Implementation
+    @Override
+    public int compareTo(Person other) {
+        return Integer.compare(this.privilege, other.privilege);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", " + identity.toString() +
+               "\nPrivilege Points: " + privilege;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || !(other instanceof Person)) {
+            return false;
+        }
+
+        Person otherPerson = (Person) other;
+        return this.name.equals(otherPerson.name) &&
+               this.identity.getPronouns().equals(otherPerson.identity.getPronouns()) &&
+               this.identity.getBackground().equals(otherPerson.identity.getBackground()) &&
+               this.privilege == otherPerson.privilege;
+    }
 }
